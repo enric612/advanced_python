cf073a09876adcbe5c3883602abb76624f83b9a0
Enric Climnent <enric@ecliment.ovh>
2015-03-14 13:54:19 +0100

    Afegida opció guardar en fitxer la eixida

--- 03-argparse/fibn.py
+++ 03-argparse/fibn.py
@@ -10,10 +10,16 @@ def Main():
     parser = argparse.ArgumentParser()
     parser.add_argument("num", help = "The Fibonnaci number " + \
                         "you wish to calculate.", type = int)
+    parser.add_argument("-o", "--output", help="Output the " + \
+                        "result to a file", action = "store_true")
     args =  parser.parse_args()
 
     result = fib(args.num)
     print "The" + str(args.num) + "th fib number is " + str(result)
+
+    if args.output:
+        f = open("fibonnaci.txt", "a")
+        f.write(str(result) + '\n')
 
 if __name__ == '__main__':
     Main()
